dc85ab4e5c1c72a4f8a2257299f05de3c64741ea
CallumHoward <callumh@cse.unsw.edu.au>
2022-06-11 11:17:34 +1000

    up to lesson 5

---
+++ src/routes/about.svelte
@@ -0,0 +1,18 @@
+<div class="about">
+	<h2>About Us</h2>
+	<p>
+		Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam a repudiandae nulla veniam
+		officiis praesentium hic voluptate, voluptatum cumque quas rerum labore, alias impedit eveniet,
+		quidem similique possimus recusandae libero.
+	</p>
+	<a href="/guides">View Guides</a>
+	<a href="/about">About</a>
+</div>
+
+<style>
+	.about {
+		text-align: center;
+		display: block;
+		margin: 20px auto;
+	}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="index">
+	<h2>Welcome</h2>
+	<p>
+		Lorem ipsum dolor sit amet consectetur adipisicing elit. Id ipsam consectetur ullam qui iusto
+		beatae repellat cum sint fugit molestias! Qui laboriosam ab ipsa amet fugiat officia,
+		necessitatibus nulla quibusdam.
+	</p>
+	<a href="guides">View Guides</a>
+	<a href="/about">About</a>
+</div>
+
+<style>
+	.index {
+		text-align: center;
+		display: block;
+		margin: 20px auto;
+	}
+</style>
